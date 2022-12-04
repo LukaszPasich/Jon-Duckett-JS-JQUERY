79b35a1d99173596229d6d5221ecd614d0e278fd
LukaszPasich <lukas.zed81@gmail.com>
2022-12-04 18:08:12 +0000

    Add c01 commit 1

--- js/add-content.js
+++ js/add-content.js
@@ -1,15 +1,22 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
+// var today = new Date();
+// var hourNow = today.getHours();
+// var greeting;
 
-if (hourNow > 18) {
-    greeting = 'Good evening!';
-} else if (hourNow > 12) {
-    greeting = 'Good afternoon!';
-} else if (hourNow > 0) {
-    greeting = 'Good morning!';
-} else {
-    greeting = 'Welcome!';
-}
+// if (hourNow > 18) {
+//     greeting = 'Good evening!';
+// } else if (hourNow > 12) {
+//     greeting = 'Good afternoon!';
+// } else if (hourNow > 0) {
+//     greeting = 'Good morning!';
+// } else {
+//     greeting = 'Welcome!';
+// }
 
-document.write('<h3>' + greeting + '</h3>');
+// document.write('<h3>' + greeting + '</h3>');
+
+
+var one = document.getElementById("console");
+one.textContent = "bbbaaaccc";
+
+
+var today = 
