cd1d9011e8bfd64cbd22fb12a85c07099f5170b2
LukaszPasich <lukas.zed81@gmail.com>
2022-12-07 23:15:32 +0000

    another commit for C01

--- js/add-content.js
+++ js/add-content.js
@@ -15,8 +15,27 @@
 // document.write('<h3>' + greeting + '</h3>');
 
 
-var one = document.getElementById("console");
-one.textContent = "bbbaaaccc";
+
+var today = new Date();
+var hourNow = today.getHours();
+var minutesNow = today.getMinutes();
+var someDay = new Date(2000000000000);
+var getDate = today.getDate();
+var getDay = today.getDay();
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+let weekDaysLength = weekDays.length;
 
 
-var today = 
+var one = document.getElementById("one");
+one.textContent = getDay;
+
+var two = document.getElementById("two");
+var hourNowText = '<p>the time now is ' + hourNow + ':' + minutesNow + '</p>';
+
+var dayTodayText =
+    '<p>Today is ' + weekDays[getDay] + '</p>' +
+    '<p>Tomorrow it is going to be ' + weekDays[getDay + 1] + '</p>';
+
+// two.innerHTML = hourNowText;
+two.innerHTML = dayTodayText;
+
